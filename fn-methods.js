d69260d599c22a66d1737958b81d11d978736f19
Anna-1987 <mag260910@gmail.com>
2025-02-10 10:22:57 +0200

    bind

--- fn-methods.js
+++ fn-methods.js
@@ -1,21 +1,57 @@
 
 // **** call и apply
 
+// const showThis = function () {
+//     console.log('showThis -> this', this);
+// };
+
+// const changeColor = function (color) {
+//     console.log('changeColor -> this', this);
+//     this.color = color;
+// };
+
+// const hat = {
+//     color: 'black',
+// };
+
+// changeColor.call(hat, 'orange');
+// console.log(hat);
+// // showThis.call(hat);// первыйм передаем объект который нужен, а все последующие произвольное количество аргументов
+
+// const sweater = {
+//     color: 'green',
+// };
+// changeColor.call(sweater, 'tomato');
+// console.log(sweater);
+
+// showThis.apply(sweater)//первыйм передаем объект который нужен, а все последующие произвольное количество мвссивов
+
+
+
+// ************---- bind------******
+
 const showThis = function () {
     console.log('showThis -> this', this);
 };
 
 const changeColor = function (color) {
     console.log('changeColor -> this', this);
-    this.color = color;
+    // this.color = color;
 };
 
 const hat = {
     color: 'black',
 };
 
-showThis.call(hat);
-
 const sweater = {
     color: 'green',
 };
+
+
+const changeHatColor = changeColor.bind(hat);
+const changeSweterColor = changeColor.bind(sweater);
+changeHatColor('yellow');
+console.log(hat);
+
+changeSweterColor('red');
+console.log(sweater);
